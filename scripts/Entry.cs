904b86897dcff859054d2a60c2b5ca0c91decf01
rainray <rayinlsee@gmail.com>
2026-03-11 16:16:32 +0800

    update mod files

---
+++ scripts/Entry.cs
@@ -0,0 +1,19 @@
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Modding;
+using MegaCrit.Sts2.Core.Models.CardPools;
+
+namespace sts1to2card.Scripts;
+
+// 必须要加的属性，用于注册Mod。字符串和初始化函数命名一致。
+[ModInitializer("Init")]
+public class Entry
+{
+
+    // 初始化函数
+    public static void Init()
+    {
+        // 传入参数随意，只要不和其他人撞车即可
+        ModHelper.AddModelToPool<SilentCardPool, src.green.cards.GreenConcentrate>();
+        ModHelper.AddModelToPool<IroncladCardPool, src.red.cards.RedEvolve>();
+    }
+}
